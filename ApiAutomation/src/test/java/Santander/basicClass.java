a72394c824ee6659c2d7d2ed84bd1c044677ba59
UshaShreePalepu <ushashreepalepu@gmail.com>
2020-07-07 15:47:18 +0100

    New project

---
+++ ApiAutomation/src/test/java/Santander/basicClass.java
@@ -0,0 +1,18 @@
+package basicClass;
+
+public class basicClass() {
+    public void setHeaders() {
+        headers = new Headers(
+                new Header(),
+                new Header(),
+                new Header(),
+                new Header()
+        );
+        setHeaders(headers);
+        }
+    @Test
+    public void testOne(){
+        requestBody = loadJsonTemplate();
+        
+        }
+        }
